72feba6b54790939e8af9b843a73b536dd5f20b3
Evgeniy <evgnec@gmail.com>
2023-06-11 17:18:09 +0300

    add ex

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,112 +1,89 @@
-import React from 'react';
-import styles from './App.module.css';
 import { Component } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Searchbar } from './Searchbar';
-import { ImageGallery } from './ImageGallery';
-import { Loader } from './Loader';
-import { Button } from './Button';
-import { getImage } from '../api';
-import { Modal } from './Modal';
+import ContactForm from './ContactForm/ContactForm';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+import { WrapperContent } from './App.styled';
+import { nanoid } from 'nanoid';
+import Notiflix from 'notiflix';
 
 export class App extends Component {
   state = {
-    searchQuery: '',
-    queryResult: [],
-    loading: false,
-    currentPage: 1,
-    isModalOpen: false,
-    modalImage: null,
-    totalQueryResult: 0,
+    contacts: [
+    ],
+    filter: '',
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { currentPage, searchQuery } = this.state;
-    const prevPage = prevState.currentPage;
-    const prevSearchQuery = prevState.searchQuery;
-
-    if (prevSearchQuery !== searchQuery || prevPage !== currentPage) {
-      this.setState({ loading: true });
-      this.handleFetch();
+  componentDidMount() {
+    const contactsLS = JSON.parse(localStorage.getItem('contacts'));
+    if (contactsLS) {
+      this.setState({contacts: contactsLS});
     }
-  }
-
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery, currentPage: 1, queryResult: [] });
   };
 
-  handleFetch = () => {
-    const { currentPage, searchQuery } = this.state;
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }  
+}
 
-    getImage(currentPage, searchQuery)
-      .then(images => {
-        if (images.hits.length === 0) {
-          toast.info('Sorry, these images were not found. Please try again.');
-          return;
-        }
 
-        this.setState(prevState => ({
-          queryResult: [...prevState.queryResult, ...images.hits],
-          showBeggin: this.statePage < Math.ceil(images.totalHits / 12),
-          totalQueryResult: images.totalHits,
-        }));
-      })
-      .catch(error => {
-        console.log(error);
-      })
-      .finally(() => this.setState({ loading: false }));
+  creatContact = ({ name, number }) => {
+        const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    if (
+      this.state.contacts.find(
+        existingContact => existingContact.name === contact.name
+      )
+    ) {
+      Notiflix.Notify.failure(`Contact ${contact.name} is already`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [contact, ...prevState.contacts],
+      }));
+      Notiflix.Notify.success(
+        `Contact ${contact.name} added to  your phonebook`
+      );
+    }
   };
 
-  incrementPage = () => {
+  deleteContact = contactId => {
     this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
-  toggleModal = () => {
-    this.setState(prevState => ({
-      isModalOpen: !prevState.isModalOpen,
-    }));
+  getFiltredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const filtredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filtredContacts;
+  };
+  hadleFilterChange = e => {
+    this.setState({ filter: e.target.value });
   };
 
-  enlargeImage = clickedImage => {
-    this.toggleModal();
-    this.setState({
-      modalImage: clickedImage,
-    });
-  };
+ 
+
   render() {
-    const {
-      searchQuery,
-      queryResult,
-      loading,
-      isModalOpen,
-      modalImage,
-      totalQueryResult,
-    } = this.state;
-
     return (
-      <div className={styles.App}>
-        <ToastContainer autoClose={3000} />
-        <Searchbar
-          onSubmit={this.handleFormSubmit}
-        />
-        {queryResult.length > 0 && (
-          <ImageGallery images={queryResult} openModal={this.enlargeImage} />
-        )}
-        {isModalOpen && (
-          <Modal
-            largeImageURL={modalImage}
-            onClose={this.toggleModal}
-            description={searchQuery}
-          />
-        )}
-        {loading && <Loader />}
-        {queryResult.length > 0 &&
-          queryResult.length !== totalQueryResult &&
-          !loading && <Button onClick={this.incrementPage} />}
-      </div>
+      <WrapperContent>
+        <ContactForm creatContact={this.creatContact} />
+        <Filter
+          value={this.state.filter}
+          onChange={this.hadleFilterChange}
+        ></Filter>
+        <Contacts
+          deleteContact={this.deleteContact}
+          contacts={this.getFiltredContacts()}
+        ></Contacts>
+      </WrapperContent>
     );
   }
 }
